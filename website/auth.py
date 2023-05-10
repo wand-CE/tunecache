abee34395895512efad6be6ea638b464eb40d9f6
wand-Ce <wanderson14117@gmail.com>
2023-05-10 03:13:28 -0400

    fix some errors made before

--- website/auth.py
+++ website/auth.py
@@ -8,7 +8,6 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import (login_user, login_required,
                          logout_user, current_user)
 from . import db
-
 from .models import User
 
 
