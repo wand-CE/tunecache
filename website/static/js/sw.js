2d8b923db34b721ea5c59ed92eb7bdc59932d38e
wand-CE <wanderson14117@gmail.com>
2024-01-07 23:09:27 -0400

    working on offline site

--- website/static/js/sw.js
+++ website/static/js/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = "tunecache_cache_v3";
+var CACHE_NAME = "tunecache_cache_v4";
 var urlsToCache = [
   "/",
   "/static/img/Tune_Logo_Nome_alternative.png",
@@ -12,8 +12,8 @@ var urlsToCache = [
   "/static/js/modules/main_conf.js",
   "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css",
   "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.4/font/bootstrap-icons.css",
-  "//code.jquery.com/jquery-3.6.0.min.js",
-  "//code.jquery.com/ui/1.13.0/jquery-ui.min.js",
+  "https://code.jquery.com/jquery-3.6.0.min.js",
+  "https://code.jquery.com/ui/1.13.0/jquery-ui.min.js",
   "https://cdnjs.cloudflare.com/ajax/libs/jqueryui-touch-punch/0.2.2/jquery.ui.touch-punch.min.js",
   "https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js",
   "https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js",
@@ -22,8 +22,6 @@ var urlsToCache = [
 ];
 
 self.addEventListener("install", function (event) {
-  self.skipWaiting();
-  // install file needed offline
   event.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
       return cache.addAll(urlsToCache);
@@ -31,13 +29,34 @@ self.addEventListener("install", function (event) {
   );
 });
 
+self.addEventListener("activate", function (event) {
+  event.waitUntil(self.clients.claim());
+});
+
 self.addEventListener("fetch", function (event) {
-  event.respondWith(
-    caches.match(event.request).then(function (response) {
-      if (response) {
-        return response;
-      }
-      return fetch(event.request);
-    })
-  );
+  if (event.request.headers.get("accept").includes("text/html")) {
+    event.respondWith(
+      fetch(event.request)
+        .then(function (response) {
+          const clonedResponse = response.clone();
+
+          caches.open(CACHE_NAME).then(function (cache) {
+            cache.put(event.request, clonedResponse);
+          });
+
+          return response;
+        })
+        .catch(function (error) {
+          console.error("Error fetching HTML document:", error);
+
+          return caches.match(event.request);
+        })
+    );
+  } else {
+    event.respondWith(
+      caches.match(event.request).then(function (response) {
+        return response || fetch(event.request);
+      })
+    );
+  }
 });
